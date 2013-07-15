2c466a76c59fb9d49cdd3bcec9e58f884eadd040
Enrique Zamudio <alterbass@gmail.com>
2013-07-15 13:53:14 -0500

    Add LGPL notice

--- src/main/java/com/solab/iso8583/util/HexCodec.java
+++ src/main/java/com/solab/iso8583/util/HexCodec.java
@@ -1,6 +1,26 @@
+/*
+ * j8583 A Java implementation of the ISO8583 protocol
+ * Copyright (C) 2007 Enrique Zamudio Lopez
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
 package com.solab.iso8583.util;
 
-/** Utility class to perform HEX encoding/decoding of values. */
+/** Utility class to perform HEX encoding/decoding of values.
+ * @author Enrique Zamudio
+ */
 public final class HexCodec {
 
 	static final char[] HEX = "0123456789ABCDEF".toCharArray();
