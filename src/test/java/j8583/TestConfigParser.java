81f178f8709c90f452193c570a6570123d4acbd0
Enrique Zamudio <alterbass@gmail.com>
2013-11-25 19:30:07 -0600

    tests for #15

--- src/test/java/j8583/TestConfigParser.java
+++ src/test/java/j8583/TestConfigParser.java
@@ -1,6 +1,7 @@
 package j8583;
 
 import com.solab.iso8583.IsoMessage;
+import com.solab.iso8583.IsoValue;
 import com.solab.iso8583.MessageFactory;
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,9 +20,28 @@ public class TestConfigParser {
     public void testParser() throws IOException {
         MessageFactory<IsoMessage> mfact = new MessageFactory<IsoMessage>();
         mfact.setConfigPath("config.xml");
+        //Headers
         Assert.assertNotNull(mfact.getIsoHeader(0x800));
         Assert.assertNotNull(mfact.getIsoHeader(0x810));
         Assert.assertEquals(mfact.getIsoHeader(0x800), mfact.getIsoHeader(0x810));
+        //Templates
+        final IsoMessage m200 = mfact.getMessageTemplate(0x200);
+        Assert.assertNotNull(m200);
+        final IsoMessage m400 = mfact.getMessageTemplate(0x400);
+        Assert.assertNotNull(m400);
+        for (int i=2; i <89; i++) {
+            IsoValue<?> v = m200.getField(i);
+            if (v == null) {
+                Assert.assertFalse(m400.hasField(i));
+            } else {
+                Assert.assertTrue(m400.hasField(i));
+                Assert.assertEquals(v, m400.getField(i));
+            }
+        }
+        Assert.assertFalse(m200.hasField(90));
+        Assert.assertTrue(m400.hasField(90));
+        Assert.assertTrue(m200.hasField(102));
+        Assert.assertFalse(m400.hasField(102));
     }
 
 }
