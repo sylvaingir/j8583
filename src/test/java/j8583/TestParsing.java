c1c6314144313a902c8405521ac54ca3f5023a4b
Enrique Zamudio <alterbass@gmail.com>
2013-11-27 11:04:35 -0600

    tests for #17 from xml config

--- src/test/java/j8583/TestParsing.java
+++ src/test/java/j8583/TestParsing.java
@@ -6,6 +6,7 @@ import java.text.ParseException;
 
 import com.solab.iso8583.IsoMessage;
 import com.solab.iso8583.IsoValue;
+import com.solab.iso8583.codecs.CompositeField;
 import com.solab.iso8583.parse.NumericParseInfo;
 import org.junit.Assert;
 import org.junit.Before;
@@ -107,4 +108,16 @@ public class TestParsing {
         Assert.assertEquals(123456, val.getValue().intValue());
     }
 
+    @Test
+    public void testComposite() throws ParseException, UnsupportedEncodingException {
+        IsoMessage m = mf.parseMessage("12000040000000000000016one  03two12345.".getBytes(), 0);
+        Assert.assertNotNull(m);
+        CompositeField f = m.getObjectValue(10);
+        Assert.assertNotNull(f);
+        Assert.assertEquals(4, f.getValues().size());
+        Assert.assertEquals("one  ", f.getObjectValue(0));
+        Assert.assertEquals("two", f.getObjectValue(1));
+        Assert.assertEquals("12345", f.getObjectValue(2));
+        Assert.assertEquals(".", f.getObjectValue(3));
+    }
 }
